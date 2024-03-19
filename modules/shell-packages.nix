b480756489d47386419982bacd7af1270d87d17b
Oliver Breitwieser <oliver@breitwieser.eu>
2024-03-20 00:28:30 +0100

    feat: introduce home-manager & split up further

---
+++ modules/shell-packages.nix
@@ -0,0 +1,39 @@
+{inputPkgs, pkgs}: 
+let
+  tmuxPlugins = import ./tmux-plugins.nix pkgs;
+in
+with pkgs; [
+    # base setup
+    bash
+    bat
+    carapace
+    curl
+    delta
+    fzf
+    gawk
+    git
+    gnumake
+    gnused
+    lsd
+    mr
+    # python3
+    ripgrep
+    ruby # only needed for neovim plugins
+    tmux # + plugins
+    ugrep
+    zsh
+    zsh-vi-mode
+
+    # own stuff
+    (callPackage (import "${inputPkgs.pydemx}") {}) # hacky way to include flake
+
+    # lsps
+    nixd
+
+    # tools
+    dua
+    duf
+    gh
+    # miller # useful tool
+    zoxide
+]
--- system/flake.nix
+++ system/flake.nix
@@ -4,16 +4,24 @@
   inputs = {
   # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     pydemx = {
       url = "github:obreitwi/pydemx";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, pydemx, ... }:
+  outputs = { self, nixpkgs, home-manager, pydemx, ... }@inputs:
     let
-      customInputs = {
-        pydemx = pydemx;
+      specialArgs = {
+        inputPkgs = { inherit pydemx; };
       };
     in
     {
@@ -21,9 +29,25 @@
         system = "x86_64-linux";
 
         modules = [
-          (import ./configuration.nix customInputs )
+          ./configuration.nix
+          {
+            _module.args = specialArgs; # make sure that regular modules can access special args as well
+          }
           ./hardware-configuration/nimir.nix
           ./hardware-customization/nimir.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.obreitwi = import ../home-manager/home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+            home-manager.extraSpecialArgs = specialArgs;
+          }
         ];
       };
     };
