3031dc471018eec53194770e48b225f7bcfba301
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-03-25 11:35:42 +0100

    doc: make sure to install nixGL on non-nixos

--- home-manager/install.sh
+++ home-manager/install.sh
@@ -2,5 +2,8 @@
 
 sourcedir="$(dirname "$(readlink -m "${BASH_SOURCE[0]}")")"
 
+# add nixGL for non-nixos installations
+nix profile install github:guibou/nixGL --impure
+
 # set up home-manager
 nix run 'home-manager/release-23.11' -- init --switch "${sourcedir}"
