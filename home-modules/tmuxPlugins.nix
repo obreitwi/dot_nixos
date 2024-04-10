7c12dfad5a3d71ac6bae4c408e511fe4459e8031
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-04-10 08:53:23 +0200

    chore(refactor): move tmux package to module

--- home-modules/tmuxPlugins.nix
+++ home-modules/tmuxPlugins.nix
@@ -6,7 +6,7 @@
 }: let
   tmuxPlugins = with pkgs-unstable.tmuxPlugins; [sensible yank gruvbox];
 in {
-  home.packages = tmuxPlugins;
+  home.packages = with pkgs-unstable; [tmux] ++ tmuxPlugins;
 
   home.file."${config.xdg.configHome}/tmux/load-plugins".text =
     lib.strings.concatMapStrings (p: "run-shell " + p.rtp + "\n") tmuxPlugins;
