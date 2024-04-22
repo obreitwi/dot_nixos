adf7c598fa62f1bc1474858c07971f75761250b0
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-04-23 00:11:00 +0200

    feat: add toggle-bluetooth-audio

---
+++ packages/toggle-bluetooth-audio.nix
@@ -0,0 +1,15 @@
+{pkgs, writeShellApplication, ...}:
+writeShellApplication {
+  name = "toggle-bluetooth-audio";
+  runtimeInputs = with pkgs; [jq pulseaudio];
+  text = ''
+    json=$(pactl --format=json list cards | jq -c '.[] | select(.name | startswith("bluez_card")) | {name, active_profile}')
+    card=$(jq -r '.name' <<<"$json")
+    profile=$(jq -r '.active_profile' <<<"$json")
+    if [[ "$profile" == "a2dp-sink" ]]; then
+      pactl set-card-profile "$card" headset-head-unit
+    else
+      pactl set-card-profile "$card" a2dp-sink
+    fi
+  '';
+}
