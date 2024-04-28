98db4c387924129c6b61574bb38359ef92a8fd48
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-04-28 22:25:03 +0200

    fix: xmobar size dependent on stalonetray width

--- home-modules/xmonad.nix
+++ home-modules/xmonad.nix
@@ -4,12 +4,27 @@
   config,
   pkgs,
   dot-desktop,
-  hostname,
   ...
-}: {
+}: 
+let
+ inherit (config.my.stalonetray) slot-size num-icons;
+in
+{
   options.my.xmonad.enable = lib.mkOption {
     default = true;
     type = lib.types.bool;
+  };
+
+  options.my.stalonetray = {
+    num-icons = lib.mkOption {
+      default = 6;
+      type = lib.types.int;
+    };
+
+    slot-size = lib.mkOption {
+      default = 17;
+      type = lib.types.int;
+    };
   };
 
   config = lib.mkIf config.my.xmonad.enable {
@@ -26,5 +41,25 @@
       toggle-bluetooth-audio
       trayer # xmonad
     ];
+
+    services.stalonetray = {
+      enable = true;
+      config = {
+        # back background
+        background = "#000000";
+        # 8 size icons with padding
+        icon_size = "${toString (slot-size - 1)}";
+        slot_size = "${toString slot-size}";
+        # up to ten icons in top right corner
+        geometry = "${toString num-icons}x1-0+0";
+        # icons are on the right
+        icon_gravity = "E";
+        skip_taskbar = true;
+        # force icon sizes
+        kludges = "force_icons_size";
+        # xmonad handles that
+        window_strut = "none";
+      };
+    };
   };
 }
