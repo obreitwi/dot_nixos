943331a70002ebf0b5a76f79074f247c8375f857
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-05-05 21:08:41 +0200

    feat: only enable azure-cli where needed

--- modules/home/azure.nix
+++ modules/home/azure.nix
@@ -2,13 +2,23 @@
   config,
   lib,
   pkgs,
+  hostname,
   ...
 }: let
+  hosts-enabled = ["mimir"];
+
   azure-cli = pkgs.azure-cli.withExtensions (with pkgs.azure-cli-extensions; [azure-devops fzf]);
 in {
-  home.packages = [azure-cli];
+  options.my.azure.enable = lib.mkOption {
+    default = builtins.elem hostname hosts-enabled;
+    type = lib.types.bool;
+  };
 
-  programs.zsh.initExtraFirst = ''
-    fpath+=(${azure-cli}/share/zsh/site-functions)
-  '';
+  config = lib.mkIf config.my.azure.enable {
+    home.packages = [azure-cli];
+
+    programs.zsh.initExtraFirst = ''
+      fpath+=(${azure-cli}/share/zsh/site-functions)
+    '';
+  };
 }
