bee3d221b274f64fdd415dc300db6f48ea503987
Oliver Breitwieser <oliver@breitwieser.eu>
2024-05-11 15:09:16 +0200

    feat: move firewall to own module

---
+++ modules/nixos/firewall.nix
@@ -0,0 +1,16 @@
+{hostname, ...}: {
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = !(builtins.elem hostname ["mucku"]);
+  networking.firewall.extraCommands = ''
+    # Silence refusal of Ubiquiti Inform updates
+        iptables \
+        --insert nixos-fw-log-refuse 1 \
+        --source 192.168.0.2 \
+        --protocol tcp \
+        --dport 8080 \
+        --jump nixos-fw-refuse
+  '';
+}
