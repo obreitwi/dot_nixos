b339d2f9c7d30ee27c683bbaaa65e96dcde5507e
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-05-12 14:33:39 +0200

    feat: move gnupg agent settings to own file

---
+++ modules/home/gnupg.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.my.gnupg.enable = lib.mkOption {
+    default = config.my.gui-apps.enable;
+    type = lib.types.bool;
+  };
+
+  config = lib.mkIf config.my.gnupg.enable {
+    home.packages = [
+      pkgs.gnupg
+    ];
+
+    services.gpg-agent = {
+      enable = true;
+
+      defaultCacheTtl = 86400;
+      defaultCacheTtlSsh = 86400;
+      maxCacheTtl = 86400;
+      maxCacheTtlSsh = 86400;
+
+      enableSshSupport = true;
+
+      pinentryPackage = pkgs.pinentry-rofi;
+    };
+
+    # NOTE not using nixos module prorams.gnupg.agent until needed!
+  };
+}
--- modules/home/gui.nix
+++ modules/home/gui.nix
@@ -14,23 +14,10 @@
       autorandr
       bluetuith
       discord
-      gnupg
       keepassxc
       neovide
       spotify
       xterm
     ];
-
-    services.gpg-agent = {
-      enable = true;
-      defaultCacheTtl = 86400;
-      defaultCacheTtlSsh = 86400;
-      maxCacheTtl = 86400;
-      maxCacheTtlSsh = 86400;
-
-      enableSshSupport = true;
-
-      pinentryPackage = pkgs.pinentry-gtk2;
-    };
   };
 }
