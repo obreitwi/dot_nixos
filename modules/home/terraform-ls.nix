71fac567da300d5b1ec9dcbb888779e08b8d772b
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-05-15 11:35:26 +0200

    feat: add terraform-ls

---
+++ modules/home/terraform-ls.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.my.terraform-ls.enable = lib.mkOption {
+    default = true;
+    type = lib.types.bool;
+  };
+
+  config = lib.mkIf config.my.terraform-ls.enable {
+    home.packages = with pkgs; [
+      terraform-ls
+    ];
+
+    programs.neovim = {
+      extraConfig = lib.mkAfter
+        /*
+        vim
+        */
+        ''
+          lua <<EOF
+            require'lspconfig'.terraformls.setup{}
+          EOF
+          autocmd BufWritePre *.tfvars lua vim.lsp.buf.format()
+          autocmd BufWritePre *.tf lua vim.lsp.buf.format()
+        '';
+    };
+  };
+}
