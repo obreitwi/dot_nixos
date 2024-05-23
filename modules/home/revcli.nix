31f293aecca6c1a386c55c24c9d0cc3b4bf9e0cc
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-05-23 21:45:56 +0200

    feat: handle revcli systemd units

--- modules/home/revcli.nix
+++ modules/home/revcli.nix
@@ -3,15 +3,47 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  deps = with pkgs; [revcli];
+  unit = {
+    Description = "Sync backlog periodically on weekdays.";
+  };
+in {
   options.my.revcli.enable = lib.mkOption {
     default = false;
     type = lib.types.bool;
   };
 
   config = lib.mkIf config.my.revcli.enable {
-    home.packages = with pkgs; [
-      revcli
-    ];
+    home.packages = deps;
+
+    systemd.user.services.revcli-sync-backlog = {
+      Unit = unit;
+      Service = {
+        ExecStart = "${pkgs.writeShellApplication {
+          name = "revcli-sync-backlog";
+          runtimeInputs = deps;
+          text = ''
+            revcli backlog sync
+          '';
+        }}/bin/revcli-sync-backlog";
+      };
+    };
+
+    systemd.user.timers.revcli-sync-backlog = {
+      Unit =
+        unit
+        // {
+          After = ["network.target"];
+        };
+      Timer = {
+        Persistent = true;
+        OnCalendar = "Mon..Fri 07..19:45:00";
+        RandomizedDelaySec = 900;
+      };
+      Install = {
+        WantedBy = ["timers.target"];
+      };
+    };
   };
 }
