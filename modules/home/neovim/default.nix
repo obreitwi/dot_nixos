078dcad2dab4d685c7710e400e7cbd6e3ea84373
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-05-26 19:58:08 +0200

    feat: (re-)add neorg

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -1,59 +1,43 @@
 {
-  lib,
-  config,
   pkgs,
-  dot-desktop,
-  hostname,
   myUtils,
   ...
 }: {
-  imports = [./treesitter.nix];
+  imports = [
+    ./neorg.nix
+    ./treesitter.nix
+  ];
 
-  options.my.neovim = {
-    neorg = lib.mkOption {
-      default = true;
-      type = lib.types.bool;
-    };
-  };
+  programs.neovim = {
+    enable = true;
 
-  config = {
-    programs.neovim = {
-      enable = true;
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = oil-nvim;
+        config =
+          myUtils.toLua
+          /*
+          lua
+          */
+          ''
+            require"oil".setup{}
+            vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+          '';
+      }
+    ];
 
-      plugins = with pkgs.vimPlugins;
-        [
-          {
-            plugin = oil-nvim;
-            config =
-              myUtils.toLua
-              /*
-              lua
-              */
-              ''
-                require"oil".setup{}
-                vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-              '';
-          }
-        ]
-        ++ lib.optionals (config.my.neovim.neorg) [
-          # neorg
-          neorg
-          neorg-telescope
-        ];
+    viAlias = true;
+    vimAlias = true;
 
-      viAlias = true;
-      vimAlias = true;
-
-      extraConfig =
-        /*
-        vim
-        */
-        ''
-          set runtimepath^=~/.vim runtimepath+=~/.vim/after
-          let &packpath = &runtimepath
-          let g:nix_enabled = 1
-          source ~/.vimrc
-        '';
-    };
+    extraConfig =
+      /*
+      vim
+      */
+      ''
+        set runtimepath^=~/.vim runtimepath+=~/.vim/after
+        let &packpath = &runtimepath
+        let g:nix_enabled = 1
+        source ~/.vimrc
+      '';
   };
 }
