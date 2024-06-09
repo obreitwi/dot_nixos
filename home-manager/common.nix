ebbc196f2e448cfd3a3ac9959eec4fef1f87ecde
Oliver Breitwieser <oliver@breitwieser.eu>
2024-06-09 14:33:53 +0200

    feat: enable go dev tools on mimir only

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -1,5 +1,6 @@
 # common configurations in home manager for both nixos and standalone
 {hostname, ...}: {
+  my.go.enable = builtins.elem hostname ["mimir"];
   my.latex.enable = builtins.elem hostname ["mimir" "mucku"];
   my.revcli.enable = builtins.elem hostname ["mimir"];
   my.terraform.enable = builtins.elem hostname ["mimir"];
