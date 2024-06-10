c7f433d81175bf2d5fc978a8b0e5ec3353a67242
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-06-10 17:12:54 +0200

    feat: add nvim.octo
    * hard-code mapleader for now until
      https://github.com/nix-community/home-manager/pull/2391 is resolved

---
+++ modules/home/neovim/octo.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  config,
+  pkgs,
+  myUtils,
+  ...
+} @ args: let
+  plugin = pkgs.vimUtils.buildVimPlugin {
+    name = "octo";
+    src = pkgs.fetchFromGitHub {
+      owner = "pwntester";
+      repo = "octo.nvim";
+      rev = "22f34582a4eb1fb221eafd0daa9eb1b2bacfb813";
+      hash = "sha256-QDeJMPBXTexTowQ0MW99Il97GHOCvGXlP2yXM1ZCqv8=";
+    };
+  };
+  config =
+    myUtils.toLua
+    /*
+    lua
+    */
+    ''
+      require("octo").setup({
+        default_merge_method = "squash",
+        enable_builtin = true,
+      })
+      -- TODO: set mapleader (https://github.com/nix-community/home-manager/pull/2391)
+      vim.keymap.set('n', '<space>O', '<cmd>Octo<cr>', {desc = 'Launch Octo'})
+    '';
+in {
+  options.my.neovim = {
+    octo = lib.mkOption {
+      default = true;
+      type = lib.types.bool;
+    };
+  };
+
+  config = lib.mkIf args.config.my.neovim.octo {
+    programs.neovim = {
+      plugins = [
+        {inherit plugin config;}
+      ];
+    };
+  };
+}
---
+++ modules/home/neovim/prefix.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  ...
+}: {
+  # TODO: Not working as intended yet, would want this config as prefix for neovim config to set leaders prior to plugin configuration.
+  programs.neovim = {
+    extraConfig =
+      lib.mkBefore
+      /*
+      vim
+      */
+      ''
+        let mapleader=" "
+        let maplocalleader=";"
+      '';
+  };
+}
