5850a216f6684ad2f80aea74e91d4dc384415e02
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-06-16 00:18:27 +0200

    feat(zsh): configure completions

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -19,12 +19,42 @@
   '';
 
   initPlugins = with pkgs;
-    writeText "zsh-init-plugins.sh" ''
+    writeText "zsh-init-plugins.sh" /* sh */ ''
       source ${zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
-      zstyle ":completion:*:git-checkout:*" sort false
-      zstyle ':completion:*:descriptions' format '[%d]'
-      zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+
+      # list of completers to use (does not work correctly)
+      # TODO: figure out how to complete on redirect 
+      zstyle ':completion:*::::' completer _expand _complete _ignored _approximate _redirect
+
       zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+
+      # disable sort when completing `git checkout`
+      zstyle ':completion:*:git-checkout:*' sort false
+
+      # set descriptions format to enable group support
+      # NOTE: don't use escape sequences here, fzf-tab will ignore them
+      zstyle ':completion:*:descriptions' format '[%d]'
+
+      # set list-colors to enable filename colorizing
+      zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+
+      # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+      zstyle ':completion:*' menu no
+
+      # preview directory's content with eza when completing cd
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd -1 --color=always $realpath'
+
+      # switch group using `<` and `>`
+      zstyle ':fzf-tab:*' switch-group '<' '>'
+
+      # apply to all command
+      zstyle ':fzf-tab:*' popup-min-size 50 8
+      # only apply to 'diff'
+      zstyle ':fzf-tab:complete:diff:*' popup-min-size 80 12
+
+      # padding for popup
+      zstyle ':fzf-tab:complete:cd:*' popup-pad 30 0
+
       # resolve issue with carapace
       zstyle ':fzf-tab:*' query-string prefix first
 
