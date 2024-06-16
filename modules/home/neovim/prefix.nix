b83cde449917942430d09fe8c1b707a0d7f7f8e8
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-06-16 11:27:34 +0200

    chore: run nix fmt

--- modules/home/neovim/prefix.nix
+++ modules/home/neovim/prefix.nix
@@ -1,7 +1,4 @@
-{
-  lib,
-  ...
-}: {
+{lib, ...}: {
   # TODO: Not working as intended yet, would want this config as prefix for neovim config to set leaders prior to plugin configuration.
   programs.neovim = {
     extraConfig =
