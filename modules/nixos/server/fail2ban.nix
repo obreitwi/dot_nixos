cb0a711a471d3ba622a97474c56cf52a8c95fd06
Oliver Breitwieser <oliver@breitwieser.eu>
2024-07-05 21:55:47 +0200

    feat: update fail2ban with jails from wiki

--- modules/nixos/server/fail2ban.nix
+++ modules/nixos/server/fail2ban.nix
@@ -1,39 +1,56 @@
 {
-  lib,
   config,
+  lib,
+  pkgs,
   ...
 }: {
-  services.fail2ban = {
-    # enable = true;
-    # Ban IP after 5 failures
-    maxretry = 5;
-    ignoreIP = [
-      # Whitelist some subnets
-      "10.0.0.0/8"
-      "8.8.8.8" # whitelist a specific IP
-      "nas.zqnr.de" # resolve the IP via DNS
-    ];
-    bantime = "1h"; # Ban IPs for one day on the first ban
-    bantime-increment = {
-      enable = true; # Enable increment of bantime after each violation
-      # formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
-      multipliers = "1 2 4 8 16 32 64";
-      maxtime = "168h"; # Do not ban for more than 1 week
-      overalljails = true; # Calculate the bantime based on all the violations
+  options = {
+    my.server.fail2ban = {
+      enable = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
+      };
     };
-    jails = {
-      # apache-nohome-iptables.settings = {
-        # # Block an IP address if it accesses a non-existent
-        # # home directory more than 5 times in 10 minutes,
-        # # since that indicates that it's scanning.
-        # filter = "apache-nohome";
-        # action = ''iptables-multiport[name=HTTP, port="http,https"]'';
-        # logpath = "/var/log/httpd/error_log*";
-        # backend = "auto";
-        # findtime = 600;
-        # bantime = 600;
-        # maxretry = 5;
-      # };
+  };
+
+  config = {
+    environment.etc = lib.mkIf config.my.server.fail2ban.enable {
+      # Defines a filter that detects URL probing by reading the Nginx access log
+      "fail2ban/filter.d/nginx-url-probe.local".text = pkgs.lib.mkDefault (pkgs.lib.mkAfter ''
+        [Definition]
+        failregex = ^<HOST>.*(GET /(wp-|admin|boaform|phpmyadmin|\.env|\.git)|\.(dll|so|cfm|asp)|(\?|&)(=PHPB8B5F2A0-3C92-11d3-A3A9-4C7B08C10000|=PHPE9568F36-D428-11d2-A769-00AA001ACF42|=PHPE9568F35-D428-11d2-A769-00AA001ACF42|=PHPE9568F34-D428-11d2-A769-00AA001ACF42)|\\x[0-9a-zA-Z]{2})
+      '');
+    };
+
+    services.fail2ban = {
+      enable = config.my.server.fail2ban.enable;
+      # Ban IP after 5 failures
+      maxretry = 5;
+      ignoreIP = [
+        # Whitelist some subnets
+        "10.0.0.0/8"
+        "8.8.8.8" # whitelist a specific IP
+        "nas.zqnr.de" # resolve the IP via DNS
+      ];
+      bantime = "1h"; # Ban IPs for one day on the first ban
+      bantime-increment = {
+        enable = true; # Enable increment of bantime after each violation
+        # formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+        multipliers = "1 2 4 8 16 32 64";
+        maxtime = "168h"; # Do not ban for more than 1 week
+        overalljails = true; # Calculate the bantime based on all the violations
+      };
+      jails = {
+        ngnix-url-probe.settings = {
+          enabled = true;
+          filter = "nginx-url-probe";
+          logpath = "/var/log/nginx/error.log";
+          action = "%(action_)s[blocktype=DROP]";
+          backend = "auto"; # Do not forget to specify this if your jail uses a log file
+          maxretry = 5;
+          findtime = 600;
+        };
+      };
     };
   };
 }
