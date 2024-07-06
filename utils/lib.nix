c17459f54ef9d67cdbfdd02698cb2ff99b5cfd1d
Oliver Breitwieser <oliver@breitwieser.eu>
2024-07-06 18:01:52 +0200

    feat: default nginx config on gentian

--- utils/lib.nix
+++ utils/lib.nix
@@ -11,4 +11,11 @@
     s = lib.strings;
   in
     s.concatStringsSep "." (l.drop 1 (s.splitString "." hostName));
+
+  nginxACME = domain: {
+    forceSSL = true;
+    sslCertificate = "/var/lib/acme/${domain}/fullchain.pem";
+    sslCertificateKey = "/var/lib/acme/${domain}/key.pem";
+    sslTrustedCertificate = "/var/lib/acme/${domain}/chain.pem";
+  };
 }
