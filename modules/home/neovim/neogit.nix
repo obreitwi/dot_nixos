d5add093391a5f33fa5d0f26ac921d080c7726d3
Oliver Breitwieser <oliver@breitwieser.eu>
2024-09-23 21:10:22 +0200

    fix: supply config as extraLuaConfig

--- modules/home/neovim/neogit.nix
+++ modules/home/neovim/neogit.nix
@@ -5,28 +5,16 @@
 }: {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
-      {
-        plugin = neogit; # required
-        config =
-          myUtils.vimLua
-          /*
-          lua
-          */
-          ''
-            require("neogit").setup()
-          '';
-      }
-      {
-        plugin = diffview-nvim; # optional
-        config =
-          myUtils.vimLua
-          /*
-          lua
-          */
-          ''
-            require("diffview").setup()
-          '';
-      }
+      neogit # required
+      diffview-nvim # optional
     ];
+    extraLuaConfig =
+      /*
+      lua
+      */
+      ''
+        require("neogit").setup()
+        require("diffview").setup()
+      '';
   };
 }
