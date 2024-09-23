18cffacc45167cab954fe26682e3fab34224bbb8
Oliver Breitwieser <oliver@breitwieser.eu>
2024-09-23 14:45:57 +0200

    feat: default setup neogit/diffview

--- modules/home/neovim/neogit.nix
+++ modules/home/neovim/neogit.nix
@@ -1,11 +1,32 @@
 {
+  myUtils,
   pkgs,
   ...
 }: {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
-      neogit # required
-      diffview-nvim # optional
+      {
+        plugin = neogit; # required
+        config =
+          myUtils.vimLua
+          /*
+          lua
+          */
+          ''
+            require("neogit").setup()
+          '';
+      }
+      {
+        plugin = diffview-nvim; # optional
+        config =
+          myUtils.vimLua
+          /*
+          lua
+          */
+          ''
+            require("diffview").setup()
+          '';
+      }
     ];
   };
 }
