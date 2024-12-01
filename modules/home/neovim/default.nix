8235a9758560481176fbb248aa4a4f1ce26df912
Oliver Breitwieser <oliver@breitwieser.eu>
2024-12-01 23:46:49 +0100

    chore(flake): update inputs & adjust to nerd-fonts split-up

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -60,6 +60,34 @@
             nnoremap <c-y> :Vista<CR>
           '';
       }
+      {
+        plugin = rustaceanvim;
+        config =
+          myUtils.vimLua
+          /*
+          lua
+          */
+          ''
+            vim.g.rustaceanvim = {
+              -- Plugin configuration
+              tools = {
+              },
+              -- LSP configuration
+              server = {
+                on_attach = function(client, bufnr)
+                  -- you can also put keymaps in here
+                end,
+                default_settings = {
+                  -- rust-analyzer language server configuration
+                  ['rust-analyzer'] = {
+                  },
+                },
+              },
+              -- DAP configuration
+              dap = {},
+            }
+          '';
+      }
     ];
 
     viAlias = true;
