5ecf0a6e69723c92b7d68a3399943018642b3011
Oliver Breitwieser <oliver@breitwieser.eu>
2025-01-02 13:54:48 +0100

    feat: switch back to adwaita and remove last gtk2 app

--- modules/home/gui/theming.nix
+++ modules/home/gui/theming.nix
@@ -17,21 +17,27 @@ lib.mkIf (config.my.gui.enable) {
   gtk.cursorTheme.name = "Bibata-Modern-Ice";
 
   # Adwaita does not work for GTK2 which then uses an inconsistent default.
-  # gtk.theme.package = pkgs.adw-gtk3;
-  # gtk.theme.name = "adw-gtk3-dark";
+  gtk.theme.package = pkgs.adw-gtk3;
+  gtk.theme.name = "adw-gtk3-dark";
+  gtk.iconTheme.package = pkgs.adwaita-icon-theme;
+  gtk.iconTheme.name = "Adwaita";
   # gtk.iconTheme.package = pkgs.gruvbox-dark-gtk;
   # gtk.iconTheme.name = "GruvboxDark";
 
   # The following themes work from GTK2 till GTK4
-  gtk.theme.package = pkgs.vimix-gtk-themes.override {
-    themeVariants = ["beryl" "doder"];
-  };
-  gtk.theme.name = "vimix-dark-beryl";
-  gtk.iconTheme.package = pkgs.adwaita-icon-theme;
-  gtk.iconTheme.name = "Adwaita";
+  # gtk.theme.package = pkgs.vimix-gtk-themes.override {
+    # themeVariants = ["beryl" "doder"];
+  # };
+  # gtk.theme.name = "vimix-dark-beryl";
+  # gtk.iconTheme.package = pkgs.adwaita-icon-theme;
+  # gtk.iconTheme.name = "Adwaita";
 
-  # eliminate warnings
-  home.packages = with pkgs; [gtk-engine-murrine];
+  home.packages = with pkgs; [
+    gtk-engine-murrine # eliminate warnings
+
+    # check settings
+    lxappearance
+  ];
 
   # qt
   qt.enable = true;
