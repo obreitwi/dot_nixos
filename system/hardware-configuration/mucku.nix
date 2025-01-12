6464c8e57f9246a7895be734244cc48af572bcaf
Oliver Breitwieser <oliver@breitwieser.eu>
2025-01-12 13:10:09 +0100

    feat(desktop): enable usb modeswitcher

--- system/hardware-configuration/mucku.nix
+++ system/hardware-configuration/mucku.nix
@@ -49,4 +49,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Prevent Fritz! WLAN stick from posing as CD-Rom first
+  hardware.usb-modeswitch.enable = true;
 }
