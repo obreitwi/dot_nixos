7e72ce68846f86f5c37e497f71bca977e36cf9a9
Oliver Breitwieser <oliver@breitwieser.eu>
2025-02-14 10:50:06 +0100

    fix: re-pin pretty-fold-nvim because it was not updated

--- modules/home/neovim/treesitter.nix
+++ modules/home/neovim/treesitter.nix
@@ -29,7 +29,14 @@
     nvim-treesitter-textobjects
     playground
     rainbow-delimiters-nvim
-    pretty-fold-nvim
+    (pretty-fold-nvim.overrideAttrs (final: prev: {
+      src = pkgs.fetchFromGitHub {
+        owner = "bbjornstad";
+        repo = "pretty-fold.nvim";
+        rev = "1eb18f228972e86b7b8f5ef33ca8091e53fb1e49";
+        hash = "sha256-0cHPm+JPGoYsjJEg3eIWv2Td1S+LQYBRbp71XPQsWMg=";
+      };
+    }))
     nvim-autopairs
     tabout-nvim
     treesj
