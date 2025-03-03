b12ef9289c76157da9ce81ac2d1de70844076089
Oliver Breitwieser <oliver@breitwieser.eu>
2025-03-04 00:07:07 +0100

    chore(nixvim): git plugins

--- modules/nixvim/plugins/tpope.nix
+++ modules/nixvim/plugins/tpope.nix
@@ -1,10 +1,8 @@
+# other tpope plugins (except for git)
 {pkgs, ...}: {
   extraPlugins = [
     pkgs.vimPlugins.vim-dispatch
     pkgs.vimPlugins.vim-eunuch
-    pkgs.vimPlugins.vim-fugitive
-    pkgs.vimPlugins.vim-git
-    pkgs.vimPlugins.vim-sleuth
     pkgs.vimPlugins.vim-sleuth
     pkgs.vimPlugins.vim-speeddating
     pkgs.vimPlugins.vim-surround
