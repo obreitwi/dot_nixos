bd974aa79535a9697574f64e27e706cccfd9fe8d
Oliver Breitwieser <oliver@breitwieser.eu>
2025-03-09 17:20:48 +0100

    fix: enable pipe-operators on desktop/servers

--- system/configuration-server.nix
+++ system/configuration-server.nix
@@ -64,7 +64,7 @@ in {
 
   nix.settings = {
     auto-optimise-store = true;
-    experimental-features = ["nix-command" "flakes"];
+    experimental-features = ["nix-command" "flakes" "pipe-operators"];
 
     max-jobs = "auto";
     cores = 0;
