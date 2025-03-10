25ff9502332901bcf30fcc332ad3b9a3a54e5ffd
Oliver Breitwieser <oliver@breitwieser.eu>
2025-03-10 20:08:25 +0100

    feat(nixvim): add grammarous

--- modules/nixvim/lang/tex.nix
+++ modules/nixvim/lang/tex.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   options = {
@@ -11,12 +12,25 @@
   };
 
   config = lib.mkIf config.my.nixvim.tex.enable {
-    plugins.cmp.settings.sources = [{ name = "texlab"; }];
+    plugins.cmp.settings.sources = [{name = "texlab";}];
     plugins.lsp.servers.texlab.enable = true;
 
     plugins.vimtex = {
       enable = true;
       settings.view_mode = "zathura";
+      # texlivePackage = pkgs.texliveFull;
     };
+
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "vim-grammarous";
+        src = pkgs.fetchFromGitHub {
+          owner = "rhysd";
+          repo = "vim-grammarous";
+          rev = "db46357465ce587d5325e816235b5e92415f8c05";
+          hash = "sha256-8kCWoXMAvzUuexxmgX4vADsJrBEwmLtl4QTjNgcujwQ=";
+        };
+      })
+    ];
   };
 }
