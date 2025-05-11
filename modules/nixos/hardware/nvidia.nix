0e8b8ada0031563db83163ad170768c28051f39e
Oliver Breitwieser <oliver@breitwieser.eu>
2025-05-11 12:34:54 +0200

    feat: initial nvidia support for desktop

---
+++ modules/nixos/hardware/nvidia.nix
@@ -0,0 +1,18 @@
+{config, lib, pkgs, ...}:
+{
+  hardware.graphics.enable = true;
+
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = { 
+    modesetting.enable = true;
+
+    powerManagement = {
+      enable = false;
+      finegrained = false;
+    };
+
+    open = true; # recommended for newer cards
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
