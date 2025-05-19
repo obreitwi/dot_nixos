0a92478f253b487a699088a7d91ab96a23f94127
Oliver Breitwieser <oliver@breitwieser.eu>
2025-05-19 23:24:46 +0200

    chore: eliminate non-fixed default values

--- modules/home/gui/fonts.nix
+++ modules/home/gui/fonts.nix
@@ -5,11 +5,11 @@
   ...
 }: {
   options.my.gui.fonts.enable = lib.mkOption {
-    default = config.my.gui.enable;
+    default = false;
     type = lib.types.bool;
   };
 
-  config = lib.mkIf config.my.gui.fonts.enable {
+  config = lib.mkIf (config.my.gui.fonts.enable || config.my.gui.enable) {
     # keep in sync with nixos fonts
     home.packages = with pkgs.nerd-fonts; [
       dejavu-sans-mono
