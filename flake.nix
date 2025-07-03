e35017adabf3c57c5a1942c276d1a9e5625c0576
Oliver Breitwieser <oliver@breitwieser.eu>
2025-07-03 22:19:09 +0200

    chore(flake): update inputs (& stable update)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     # not needed right now (no sudo)
     #nix-darwin = {
