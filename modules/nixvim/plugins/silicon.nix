8d5bdec38b5d92c00481abe2747e834b97a51b06
Oliver Breitwieser <oliver@breitwieser.eu>
2025-07-09 22:47:03 +0200

    feat(silicon): configure vim plugin

--- modules/nixvim/plugins/silicon.nix
+++ modules/nixvim/plugins/silicon.nix
@@ -23,5 +23,35 @@ in {
     extraPlugins = [
       nvim-silicon
     ];
+
+    extraConfigLua = ''
+      require('nvim-silicon').setup {
+        font = "IosevkaTerm NF=34;Noto Color Emoji=34",
+        background = '#00000000',
+        no_window_controls = true,
+        to_clipboard = true,
+        theme = "gruvbox-dark",
+        shadow_blur_radius = 8,
+        -- language = "PHP Source",
+        pad_horiz = 50,
+        pad_vert = 40,
+        language = function()
+          local lang = nil
+          if vim.bo.filetype == nil or vim.bo.filetype == "" then
+            -- if we cannot determine the filetype supply no default argument
+            lang = vim.fn.input("Language: ", "")
+          else
+            -- otherwise have the filetype as preset for most cases
+            lang = vim.fn.input("Language: ", vim.bo.filetype)
+          end
+          if lang and lang ~= "" then
+            return lang
+          else
+            -- dialog was cancelled
+            return "md"
+          end
+        end,
+      }
+    '';
   };
 }
