f7b0939bf7cc6051f6946dc6e1732a8a4e83d24d
Oliver Breitwieser <oliver@breitwieser.eu>
2025-07-17 10:57:07 +0200

    feat(nixvim): add angular lsp

--- modules/nixvim/lang/typescript.nix
+++ modules/nixvim/lang/typescript.nix
@@ -3,22 +3,44 @@
   config,
   ...
 }: {
-  options.my.nixvim.lang.typescript = lib.mkOption {
-    default = false;
-    type = lib.types.bool;
-  };
-
-  config = lib.mkIf (config.my.nixvim.lang.all || config.my.nixvim.lang.typescript) {
-    plugins.lsp.servers = {
-      eslint = {
-        enable = true;
-        settings.format = true;
+  options.my.nixvim.lang = {
+    angular = {
+      enable = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
       };
-
-      ts_ls = {
-        enable = true;
-        settings.format = false;
+      disable = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
       };
     };
+    typescript = lib.mkOption {
+      default = false;
+      type = lib.types.bool;
+    };
   };
+
+  config = lib.mkMerge [
+    (lib.mkIf (config.my.nixvim.lang.all || config.my.nixvim.lang.typescript) {
+      plugins.lsp.servers = {
+        eslint = {
+          enable = true;
+          settings.format = true;
+        };
+
+        ts_ls = {
+          enable = true;
+          settings.format = false;
+        };
+      };
+    })
+
+    (lib.mkIf ((config.my.nixvim.lang.all || config.my.nixvim.lang.angular.enable) && !config.my.nixvim.lang.angular.disable) {
+      plugins.lsp.servers = {
+        angularls = {
+          enable = true;
+        };
+      };
+    })
+  ];
 }
