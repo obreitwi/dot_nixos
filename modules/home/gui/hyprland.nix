7394f9d09684e581e4c597279038fd61021efeec
Oliver Breitwieser <oliver@breitwieser.eu>
2025-10-16 01:14:34 +0200

    feat(hyprland): more keybindings

--- modules/home/gui/hyprland.nix
+++ modules/home/gui/hyprland.nix
@@ -7,7 +7,7 @@
   ...
 }: {
   options.my.gui.hyprland.enable = lib.mkOption {
-    default = true;
+    default = false;
     type = lib.types.bool;
   };
 
@@ -31,18 +31,70 @@
       settings = {
         debug.disable_logs = true; # TODO: Disable once done.
 
+        binds = {
+          allow_workspace_cycles = true;
+          drag_threshold = 10; # Fire a drag event only after dragging for more than 10px
+        };
+
+        bindm = [
+          "Super, mouse:272, movewindow" # Super + LMB: Move a window by dragging more than 10px.
+        ];
+        bindc = [
+          "Super, mouse:272, togglefloating" # Super + LMB: Floats a window by clicking
+        ];
+
         bind = [
           "Super Shift, Return, exec, alacritty"
           "Super, M, exec, rofi -show run -sort"
 
           "Super Shift, F4, exit"
+
+          "Super Shift, C, killactive,"
+          "Super Shift, Space, togglefloating,"
+
+          "Super, P, fullscreen"
+
+          # Move focus with modifier + arrow keys
+          "Super, H, movefocus, l"
+          "Super, L, movefocus, r"
+          "Super, K, movefocus, u"
+          "Super, J, movefocus, d"
+
+          # Move window with modifier + Shift + arrow keys
+          "Super Shift, H, movewindow, l"
+          "Super Shift, L, movewindow, r"
+          "Super Shift, K, movewindow, u"
+          "Super Shift, J, movewindow, d"
+
+          # Switch workspaces with modifier + [0-9]
+          "Super, 1, workspace, 1"
+          "Super, 2, workspace, 2"
+          "Super, 3, workspace, 3"
+          "Super, 4, workspace, 4"
+          "Super, 5, workspace, 5"
+          "Super, 6, workspace, 6"
+          "Super, 7, workspace, 7"
+          "Super, 8, workspace, 8"
+          "Super, 9, workspace, 9"
+          "Super, 0, workspace, 10"
+
+          # Navigate between workspaces with modifier + Alt + arrow keys
+          "Super Ctrl, H, workspace, e-1" # Go to workspace on the left
+          "Super Ctrl, L, workspace, e+1" # Go to workspace on the right
+
+          # Move between monitors
+          "Super, S, movecurrentworkspacetomonitor, l"
+          "Super, D, movecurrentworkspacetomonitor, r"
         ];
 
         general = {
-          gaps_in = 3;
-          gaps_out = 3;
+          gaps_in = 5;
+          gaps_out = 5;
+          radius = 5;
           border_size = 1;
+        };
 
+        decorations = {
           follow_mouse = 2; # focus follows on click
         };
 
