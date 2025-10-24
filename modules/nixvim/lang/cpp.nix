2bd73d7f3d86fc1ad041a21a39ad5c23c7363594
Oliver Breitwieser <oliver@breitwieser.eu>
2025-10-24 21:55:35 +0200

    feat(nixvim): add lsps for c++

--- modules/nixvim/lang/cpp.nix
+++ modules/nixvim/lang/cpp.nix
@@ -1,10 +1,26 @@
-{utils, ...}: let
+{
+  config,
+  lib,
+  utils,
+  ...
+}: let
   inherit (utils) autoCmdFT;
 in {
-  autoCmd = [
-    (autoCmdFT {
-      lang = "cpp";
-      command = "setlocal cinoptions=g0,hs,N-s,+0";
-    })
-  ];
+  options.my.nixvim.lang.cpp = lib.mkOption {
+    default = false;
+    type = lib.types.bool;
+  };
+
+  config = lib.mkIf (config.my.nixvim.lang.all || config.my.nixvim.lang.cpp) {
+    autoCmd = [
+      (autoCmdFT {
+        lang = "cpp";
+        command = "setlocal cinoptions=g0,hs,N-s,+0";
+      })
+    ];
+
+    plugins.clangd-extensions.enable = true;
+    plugins.lsp.servers.clangd.enable = true;
+    plugins.lsp.servers.mesonlsp.enable = true;
+  };
 }
