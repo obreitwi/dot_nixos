a315399ebfeafb3c8ea384ec015a157e929b6170
Oliver Breitwieser <oliver@breitwieser.eu>
2025-10-26 18:02:49 +0100

    chore(flake): update inputs

--- modules/nixvim/treesitter/default.nix
+++ modules/nixvim/treesitter/default.nix
@@ -47,83 +47,86 @@ in {
 
     treesitter-textobjects = {
       enable = true;
-      select = {
-        enable = true;
-        keymaps = {
-          # You can use the capture groups defined in textobjects.scm
-          "af" = "@function.outer";
-          "if" = "@function.inner";
-          "ac" = "@class.outer";
-          # you can optionally set descriptions to the mappings (used in the desc parameter of nvim_buf_set_keymap
-          "ic" = {
-            query = "@class.inner";
-            desc = "Select inner part of a class region";
+
+      settings = {
+        select = {
+          enable = true;
+          keymaps = {
+            # You can use the capture groups defined in textobjects.scm
+            "af" = "@function.outer";
+            "if" = "@function.inner";
+            "ac" = "@class.outer";
+            # you can optionally set descriptions to the mappings (used in the desc parameter of nvim_buf_set_keymap
+            "ic" = {
+              query = "@class.inner";
+              desc = "Select inner part of a class region";
+            };
           };
+          # You can choose the select mode (default is charwise 'v')
+          selectionModes = {
+            "@parameter.outer" = "v"; # charwise
+            "@function.outer" = "V"; # linewise
+            "@class.outer" = "<c-v>"; # blockwise
+          };
+          # If you set this to `true` (default is `false`) then any textobject is
+          # extended to include preceding xor succeeding whitespace. Succeeding
+          # whitespace has priority in order to act similarly to eg the built-in
+          # `ap`.
+          includeSurroundingWhitespace = true;
         };
-        # You can choose the select mode (default is charwise 'v')
-        selectionModes = {
-          "@parameter.outer" = "v"; # charwise
-          "@function.outer" = "V"; # linewise
-          "@class.outer" = "<c-v>"; # blockwise
-        };
-        # If you set this to `true` (default is `false`) then any textobject is
-        # extended to include preceding xor succeeding whitespace. Succeeding
-        # whitespace has priority in order to act similarly to eg the built-in
-        # `ap`.
-        includeSurroundingWhitespace = true;
-      };
-      move = {
-        enable = true;
-        setJumps = true; # whether to set jumps in the jumplist
-        gotoNextStart = {
-          "]m" = "@function.outer";
-          "]]" =
-            # lua
-            ''{ query = "@class.outer", desc = "Next class start" }'';
-          #
-          # You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-          "]o" = "@loop.*";
-          # ["]o"] = { query = { "@loop.inner"; "@loop.outer" } }
-          #
-          # You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-          # Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-          "]s" =
-            # lua
-            ''
-              {
-                          query = "@local.scope";
-                          query_group = "locals";
-                          desc = "Next scope";
-                        }'';
-          "]z" =
-            # lua
-            ''
-              {
-                          query = "@fold",
-                          query_group = "folds",
-                          desc = "Next fold",
-                        }'';
-        };
-        gotoNextEnd = {
-          "]M" = "@function.outer";
-          "][" = "@class.outer";
-        };
-        gotoPreviousStart = {
-          "[m" = "@function.outer";
-          "[[" = "@class.outer";
-        };
-        gotoPreviousEnd = {
-          "[M" = "@function.outer";
-          "[]" = "@class.outer";
-        };
-        # Below will go to either the start or the end; whichever is closer.
-        # Use if you want more granular movements
-        # Make it even more gradual by adding multiple queries and regex.
-        gotoNext = {
-          "]c" = "@conditional.outer";
-        };
-        gotoPrevious = {
-          "[c" = "@conditional.outer";
+        move = {
+          enable = true;
+          setJumps = true; # whether to set jumps in the jumplist
+          gotoNextStart = {
+            "]m" = "@function.outer";
+            "]]" =
+              # lua
+              ''{ query = "@class.outer", desc = "Next class start" }'';
+            #
+            # You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+            "]o" = "@loop.*";
+            # ["]o"] = { query = { "@loop.inner"; "@loop.outer" } }
+            #
+            # You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+            # Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+            "]s" =
+              # lua
+              ''
+                {
+                            query = "@local.scope";
+                            query_group = "locals";
+                            desc = "Next scope";
+                          }'';
+            "]z" =
+              # lua
+              ''
+                {
+                            query = "@fold",
+                            query_group = "folds",
+                            desc = "Next fold",
+                          }'';
+          };
+          gotoNextEnd = {
+            "]M" = "@function.outer";
+            "][" = "@class.outer";
+          };
+          gotoPreviousStart = {
+            "[m" = "@function.outer";
+            "[[" = "@class.outer";
+          };
+          gotoPreviousEnd = {
+            "[M" = "@function.outer";
+            "[]" = "@class.outer";
+          };
+          # Below will go to either the start or the end; whichever is closer.
+          # Use if you want more granular movements
+          # Make it even more gradual by adding multiple queries and regex.
+          gotoNext = {
+            "]c" = "@conditional.outer";
+          };
+          gotoPrevious = {
+            "[c" = "@conditional.outer";
+          };
         };
       };
     };
