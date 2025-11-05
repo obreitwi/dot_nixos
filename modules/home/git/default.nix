1c1ac9959f4983e72bf53216186c439b908a809c
Oliver Breitwieser <oliver@breitwieser.eu>
2025-11-05 16:48:46 +0100

    feat(git): add hook for jira/rev ids

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -13,6 +13,38 @@
       name = Oliver Breitwieser
       signingkey = BF1B0895E8BD4A52
     '';
+
+  hooks.prepare-commit-msg = pkgs.writeShellApplication {
+    name = "prepare-commit-msg";
+    runtimeInputs = [pkgs.gawk];
+    text = ''
+      COMMIT_MSG_FILE=$1
+      # COMMIT_SOURCE=$2
+      # SHA1=$3
+
+      if grep -iq "^\(Jira\|^Rev\):" "$COMMIT_MSG_FILE"; then
+        # Story IDs exist, no need to do anything
+        exit 0
+      fi
+
+      id=$(git branch --show-current | cut -d / -f 2 | grep "[A-Z]\+-[A-Z0-9]\+" || true)
+      if [ -z "$id" ]; then
+        # branch-name does not contain valid rev/jira id -> cannot add
+        exit 0
+      fi
+
+      gawk -v "id=Rev: ''$id" '/^#/ { if (added==0) { printf "%s\n", id; added=1 } } { print } END { if (added=0) { print id } }' <"$COMMIT_MSG_FILE" >"''${COMMIT_MSG_FILE}.tmp"
+
+      mv "''${COMMIT_MSG_FILE}.tmp" "$COMMIT_MSG_FILE"
+    '';
+  };
+
+  hooksFolder = pkgs.linkFarm "hooks" [
+    {
+      name = "prepare-commit-msg";
+      path = "${hooks.prepare-commit-msg}/bin/prepare-commit-msg";
+    }
+  ];
 in {
   home.file.".gitconfig".text =
     # gitconfig
@@ -21,6 +53,8 @@ in {
         path = ${gitconfig_private}
       [includeIf "gitdir:~/projects/"]
         path = ${gitconfig_private}
+      [core]
+        hooksPath = ${hooksFolder}
     ''
     + gitconfig;
 
