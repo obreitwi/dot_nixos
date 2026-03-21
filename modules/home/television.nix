9205b225d4301f502086ea0dfdb7813ded3fbbdb
Oliver Breitwieser <oliver@breitwieser.eu>
2026-03-21 22:26:29 +0100

    feat: add television and nix-search-tv

---
+++ modules/home/television.nix
@@ -0,0 +1,26 @@
+{nixvimOptions, ...}: {
+  programs.television = {
+    enable = true;
+
+    enableZshIntegration = false; # still using ctrl-t
+
+    settings = {
+      theme = "gruvbox-dark";
+    };
+  };
+
+  programs.nix-search-tv = {
+    enable = true;
+    enableTelevisionIntegration = true;
+
+    settings = {
+      experimental.options_file = {
+        nixvim = nixvimOptions;
+      };
+    };
+  };
+
+  programs.zsh.shellAliases = {
+    "ns" = "tv nix-search-tv";
+  };
+}
